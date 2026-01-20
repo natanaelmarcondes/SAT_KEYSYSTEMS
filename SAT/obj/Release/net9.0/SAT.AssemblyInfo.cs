675733be5bb472bb73c60dc067d367a6bc00403b
natanael <natanael@keysystems.com.br>
2026-01-20 14:34:09 -0300

    Adicionado botão voltar, manter preenchido a busca

--- SAT/obj/Release/net9.0/SAT.AssemblyInfo.cs
+++ SAT/obj/Release/net9.0/SAT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SAT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf90d7635c8c4a91a31dd41076aaffa5d9de4a0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07cf561efe6d87c4019de9bc51d29bfd6c8ed68")]
 [assembly: System.Reflection.AssemblyProductAttribute("SAT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SAT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
